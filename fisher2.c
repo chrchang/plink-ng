37e545c707d314ec03f5edb1739b13a28694312d
Christopher Chang <chrchang523@gmail.com>
2013-02-11 12:42:42 +0800

    final-ish version of fisher23().  now to create variants of fisher22 to support mass association testing

--- fisher2.c
+++ fisher2.c
@@ -32,10 +32,10 @@ double fisher22(uint32_t m11, uint32_t m12, uint32_t m21, uint32_t m22) {
   double cprob = 0;
   double tail_stop;
   uint32_t uii;
-  int64_t cur11; // signed integers convert to doubles more easily
-  int64_t cur12;
-  int64_t cur21;
-  int64_t cur22;
+  double cur11; // signed integers convert to doubles more easily
+  double cur12;
+  double cur21;
+  double cur22;
   // Ensure we are left of the distribution center, m11 <= m22, and m12 <= m21.
   if (m12 > m21) {
     uii = m12;
@@ -59,8 +59,12 @@ double fisher22(uint32_t m11, uint32_t m12, uint32_t m21, uint32_t m22) {
   cur12 = m12;
   cur21 = m21;
   cur22 = m22;
-  while (cur12) {
-    cur_prob *= ((double)((cur12--) * (cur21--))) / ((double)((++cur11) * (++cur22)));
+  while (cur12 > 0.5) {
+    cur11 += 1;
+    cur22 += 1;
+    cur_prob *= (cur12 * cur21) / (cur11 * cur22);
+    cur12 -= 1;
+    cur21 -= 1;
     if (cur_prob == INFINITY) {
       return 0;
     }
@@ -73,30 +77,38 @@ double fisher22(uint32_t m11, uint32_t m12, uint32_t m21, uint32_t m22) {
   if (cprob == 0) {
     return 1;
   }
-  if (cur12) {
+  if (cur12 > 0.5) {
     tail_stop = tprob * DOUBLE_PREC_LIMIT;
     do {
-      cur_prob *= ((double)((cur12--) * (cur21--))) / ((double)((++cur11) * (++cur22)));
+      cur11 += 1;
+      cur22 += 1;
+      cur_prob *= (cur12 * cur21) / (cur11 * cur22);
+      cur12 -= 1;
+      cur21 -= 1;
       if (cur_prob < tail_stop) {
 	break;
       }
       tprob += cur_prob;
-    } while (cur12);
+    } while (cur12 > 0.5);
   }
   if (m11) {
     cur11 = m11;
     cur12 = m12;
     cur21 = m21;
     cur22 = m22;
-    cur_prob = 1;
+    cur_prob = 1 - SMALL_EPSILON;
     tail_stop = tprob * DOUBLE_PREC_LIMIT;
     do {
-      cur_prob *= ((double)((cur11--) * (cur22--))) / ((double)((++cur12) * (++cur21)));
+      cur12 += 1;
+      cur21 += 1;
+      cur_prob *= (cur11 * cur22) / (cur12 * cur21);
+      cur11 -= 1;
+      cur22 -= 1;
       if (cur_prob < tail_stop) {
         return tprob / (cprob + tprob);
       }
       tprob += cur_prob;
-    } while (cur11);
+    } while (cur11 > 0.5);
   }
   return tprob / (cprob + tprob);
 }
@@ -108,11 +120,11 @@ int32_t fisher23_tailsum(double tail_stop, double* base_probp, double* saved12p,
   double tmp13 = *saved13p;
   double tmp22 = *saved22p;
   double tmp23 = *saved23p;
-  double prev_prob;
   double tmps12;
   double tmps13;
   double tmps22;
   double tmps23;
+  double prev_prob;
   // identify beginning of tail
   if (right_side) {
     if (cur_prob > 1) {
@@ -127,11 +139,11 @@ int32_t fisher23_tailsum(double tail_stop, double* base_probp, double* saved12p,
 	tmp13 -= 1;
 	tmp22 -= 1;
       } while (cur_prob > 1);
-      *saved12p = tmp12;
-      *saved13p = tmp13;
-      *saved22p = tmp22;
-      *saved23p = tmp23;
       *base_probp = cur_prob;
+      tmps12 = tmp12;
+      tmps13 = tmp13;
+      tmps22 = tmp22;
+      tmps23 = tmp23;
     } else {
       tmps12 = tmp12;
       tmps13 = tmp13;
@@ -141,7 +153,7 @@ int32_t fisher23_tailsum(double tail_stop, double* base_probp, double* saved12p,
 	prev_prob = cur_prob;
 	tmp13 += 1;
 	tmp22 += 1;
-	cur_prob = (prev_prob * tmp12 * tmp23) / (tmp13 * tmp22);
+	cur_prob *= (tmp12 * tmp23) / (tmp13 * tmp22);
 	if (cur_prob < prev_prob) {
 	  return 1;
 	}
@@ -155,14 +167,6 @@ int32_t fisher23_tailsum(double tail_stop, double* base_probp, double* saved12p,
       prev_prob = cur_prob;
       cur_prob = *base_probp;
       *base_probp = prev_prob;
-      *saved12p = tmp12;
-      *saved13p = tmp13;
-      *saved22p = tmp22;
-      *saved23p = tmp23;
-      tmp12 = tmps12;
-      tmp13 = tmps13;
-      tmp22 = tmps22;
-      tmp23 = tmps23;
     }
   } else {
     if (cur_prob > 1) {
@@ -177,11 +181,11 @@ int32_t fisher23_tailsum(double tail_stop, double* base_probp, double* saved12p,
 	tmp12 -= 1;
 	tmp23 -= 1;
       } while (cur_prob > 1);
-      *saved12p = tmp12;
-      *saved13p = tmp13;
-      *saved22p = tmp22;
-      *saved23p = tmp23;
       *base_probp = cur_prob;
+      tmps12 = tmp12;
+      tmps13 = tmp13;
+      tmps22 = tmp22;
+      tmps23 = tmp23;
     } else {
       tmps12 = tmp12;
       tmps13 = tmp13;
@@ -191,7 +195,7 @@ int32_t fisher23_tailsum(double tail_stop, double* base_probp, double* saved12p,
 	prev_prob = cur_prob;
 	tmp12 += 1;
 	tmp23 += 1;
-	cur_prob = (prev_prob * tmp13 * tmp22) / (tmp12 * tmp23);
+	cur_prob *= (tmp13 * tmp22) / (tmp12 * tmp23);
 	if (cur_prob < prev_prob) {
 	  return 1;
 	}
@@ -205,34 +209,30 @@ int32_t fisher23_tailsum(double tail_stop, double* base_probp, double* saved12p,
       prev_prob = cur_prob;
       cur_prob = *base_probp;
       *base_probp = prev_prob;
-      *saved12p = tmp12;
-      *saved13p = tmp13;
-      *saved22p = tmp22;
-      *saved23p = tmp23;
-      tmp12 = tmps12;
-      tmp13 = tmps13;
-      tmp22 = tmps22;
-      tmp23 = tmps23;
     }
   }
+  *saved12p = tmp12;
+  *saved13p = tmp13;
+  *saved22p = tmp22;
+  *saved23p = tmp23;
   // sum tail to floating point precision limit
   if (right_side) {
     while (cur_prob > tail_stop) {
       total += cur_prob;
-      tmp12 += 1;
-      tmp23 += 1;
-      cur_prob *= (tmp13 * tmp22) / (tmp12 * tmp23);
-      tmp13 -= 1;
-      tmp22 -= 1;
+      tmps12 += 1;
+      tmps23 += 1;
+      cur_prob *= (tmps13 * tmps22) / (tmps12 * tmps23);
+      tmps13 -= 1;
+      tmps22 -= 1;
     }
   } else {
     while (cur_prob > tail_stop) {
       total += cur_prob;
-      tmp13 += 1;
-      tmp22 += 1;
-      cur_prob *= (tmp12 * tmp23) / (tmp13 * tmp22);
-      tmp12 -= 1;
-      tmp23 -= 1;
+      tmps13 += 1;
+      tmps22 += 1;
+      cur_prob *= (tmps12 * tmps23) / (tmps13 * tmps22);
+      tmps12 -= 1;
+      tmps23 -= 1;
     }
   }
   *totalp = total;
@@ -339,37 +339,39 @@ double fisher23(uint32_t m11, uint32_t m12, uint32_t m13, uint32_t m21, uint32_t
     tmp13 = savedr13;
     tmp22 = savedr22;
     tmp23 = savedr23;
-    if ((tmp12 > 0.5) && (tmp23 > 0.5)) {
-      while (1) {
-	tmp13 += 1;
-	tmp22 += 1;
-	cur_prob *= (tmp12 * tmp23) / (tmp13 * tmp22);
-	tmp12 -= 1;
-	tmp23 -= 1;
-	if (cur_prob <= 1) {
-	  break;
-	}
-	cprob += cur_prob;
-	if (cprob == INFINITY) {
-	  return 0;
-	}
+    // m12 and m23 must be nonzero
+    dxx = tmp12 * tmp23;
+    do {
+      tmp13 += 1;
+      tmp22 += 1;
+      cur_prob *= dxx / (tmp13 * tmp22);
+      tmp12 -= 1;
+      tmp23 -= 1;
+      if (cur_prob <= 1) {
+	tprob += cur_prob;
+	break;
       }
-    }
+      cprob += cur_prob;
+      if (cprob == INFINITY) {
+	return 0;
+      }
+      dxx = tmp12 * tmp23;
+      // must enforce tmp12 >= 0 and tmp23 >= 0 since we're saving these
+    } while (dxx > 0.5);
     savedl12 = tmp12;
     savedl13 = tmp13;
     savedl22 = tmp22;
     savedl23 = tmp23;
     base_probl = cur_prob;
-    tprob += cur_prob;
     tail_stop = tprob * DOUBLE_PREC_LIMIT;
-    while (cur_prob > tail_stop) {
+    do {
       tmp13 += 1;
       tmp22 += 1;
       cur_prob *= (tmp12 * tmp23) / (tmp13 * tmp22);
       tmp12 -= 1;
       tmp23 -= 1;
       tprob += cur_prob;
-    }
+    } while (cur_prob > tail_stop);
     tmp12 = savedr12;
     tmp13 = savedr13;
     tmp22 = savedr22;
@@ -390,55 +392,63 @@ double fisher23(uint32_t m11, uint32_t m12, uint32_t m13, uint32_t m21, uint32_t
     savedl13 = m13;
     savedl22 = m22;
     savedl23 = m23;
-    tmp12 = savedl12;
-    tmp13 = savedl13;
-    tmp22 = savedl22;
-    tmp23 = savedl23;
-    if ((tmp13 > 0.5) && (tmp22 > 0.5)) {
-      while (1) {
+    if (!((((uint64_t)m12) * m23) + (((uint64_t)m13) * m22))) {
+      base_probr = cur_prob;
+      savedr12 = savedl12;
+      savedr13 = savedl13;
+      savedr22 = savedl22;
+      savedr23 = savedl23;
+    } else {
+      tmp12 = savedl12;
+      tmp13 = savedl13;
+      tmp22 = savedl22;
+      tmp23 = savedl23;
+      dxx = tmp13 * tmp22;
+      do {
 	tmp12 += 1;
 	tmp23 += 1;
-	cur_prob *= (tmp13 * tmp22) / (tmp12 * tmp23);
+	cur_prob *= dxx / (tmp12 * tmp23);
 	tmp13 -= 1;
 	tmp22 -= 1;
 	if (cur_prob <= 1) {
+	  tprob += cur_prob;
 	  break;
 	}
 	cprob += cur_prob;
 	if (cprob == INFINITY) {
 	  return 0;
 	}
-      }
+	dxx = tmp13 * tmp22;
+      } while (dxx > 0.5);
+      savedr12 = tmp12;
+      savedr13 = tmp13;
+      savedr22 = tmp22;
+      savedr23 = tmp23;
+      base_probr = cur_prob;
+      tail_stop = tprob * DOUBLE_PREC_LIMIT;
+      do {
+	tmp12 += 1;
+	tmp23 += 1;
+	cur_prob *= (tmp13 * tmp22) / (tmp12 * tmp23);
+	tmp13 -= 1;
+	tmp22 -= 1;
+	tprob += cur_prob;
+      } while (cur_prob > tail_stop);
+      tmp12 = savedl12;
+      tmp13 = savedl13;
+      tmp22 = savedl22;
+      tmp23 = savedl23;
+      tail_stop = tprob * DOUBLE_PREC_LIMIT;
+      cur_prob = base_probl;
+      do {
+	tmp13 += 1;
+	tmp22 += 1;
+	cur_prob *= (tmp12 * tmp23) / (tmp13 * tmp22);
+	tmp12 -= 1;
+	tmp23 -= 1;
+	tprob += cur_prob;
+      } while (cur_prob > tail_stop);
     }
-    savedr12 = tmp12;
-    savedr13 = tmp13;
-    savedr22 = tmp22;
-    savedr23 = tmp23;
-    base_probr = cur_prob;
-    tprob += cur_prob;
-    tail_stop = tprob * DOUBLE_PREC_LIMIT;
-    while (cur_prob > tail_stop) {
-      tmp12 += 1;
-      tmp23 += 1;
-      cur_prob *= (tmp13 * tmp22) / (tmp12 * tmp23);
-      tmp13 -= 1;
-      tmp22 -= 1;
-      tprob += cur_prob;
-    }
-    tmp12 = savedl12;
-    tmp13 = savedl13;
-    tmp22 = savedl22;
-    tmp23 = savedl23;
-    tail_stop = tprob * DOUBLE_PREC_LIMIT;
-    cur_prob = base_probl;
-    do {
-      tmp13 += 1;
-      tmp22 += 1;
-      cur_prob *= (tmp12 * tmp23) / (tmp13 * tmp22);
-      tmp12 -= 1;
-      tmp23 -= 1;
-      tprob += cur_prob;
-    } while (cur_prob > tail_stop);
   }
   row_prob = tprob + cprob;
   orig_base_probl = base_probl;
@@ -452,7 +462,7 @@ double fisher23(uint32_t m11, uint32_t m12, uint32_t m13, uint32_t m21, uint32_t
   orig_savedr13 = savedr13;
   orig_savedr22 = savedr22;
   orig_savedr23 = savedr23;
-  ukk = 0;
+  tail_stop = tprob * DOUBLE_PREC_LIMIT;
   for (; dir < 2; dir++) {
     cur11 = m11;
     cur21 = m21;
@@ -468,15 +478,21 @@ double fisher23(uint32_t m11, uint32_t m12, uint32_t m13, uint32_t m21, uint32_t
       savedr13 = orig_savedr13;
       savedr22 = orig_savedr22;
       savedr23 = orig_savedr23;
+      ukk = m11;
+      if (ukk > m22 + m23) {
+	ukk = m22 + m23;
+      }
+    } else {
+      ukk = m21;
+      if (ukk > m12 + m13) {
+	ukk = m12 + m13;
+      }
     }
-    while (1) {
+    ukk++;
+    while (--ukk) {
       if (dir) {
-	if ((cur11 < 0.5) || (savedl22 + savedl23 < 0.5)) {
-	  ukk = 1;
-	  break;
-	}
 	cur21 += 1;
-	if (savedl23 > savedl22) {
+	if (savedl23) {
 	  savedl13 += 1;
 	  row_prob *= (cur11 * (savedl22 + savedl23)) / (cur21 * (savedl12 + savedl13));
 	  base_probl *= (cur11 * savedl23) / (cur21 * savedl13);
@@ -489,41 +505,36 @@ double fisher23(uint32_t m11, uint32_t m12, uint32_t m13, uint32_t m21, uint32_t
 	}
 	cur11 -= 1;
       } else {
-	if ((cur21 < 0.5) || (savedl12 + savedl13 < 0.5)) {
-	  ukk = 1;
-	  break;
-	}
 	cur11 += 1;
-	if (savedl13 > savedl12) {
-	  savedl23 += 1;
-	  row_prob *= (cur21 * (savedl12 + savedl13)) / (cur11 * (savedl22 + savedl23));
-	  base_probl *= (cur21 * savedl13) / (cur11 * savedl23);
-	  savedl13 -= 1;
-	} else {
+	if (savedl12) {
 	  savedl22 += 1;
 	  row_prob *= (cur21 * (savedl12 + savedl13)) / (cur11 * (savedl22 + savedl23));
 	  base_probl *= (cur21 * savedl12) / (cur11 * savedl22);
 	  savedl12 -= 1;
+	} else {
+	  savedl23 += 1;
+	  row_prob *= (cur21 * (savedl12 + savedl13)) / (cur11 * (savedl22 + savedl23));
+	  base_probl *= (cur21 * savedl13) / (cur11 * savedl23);
+	  savedl13 -= 1;
 	}
 	cur21 -= 1;
       }
-      tail_stop = tprob * DOUBLE_PREC_LIMIT;
       if (fisher23_tailsum(tail_stop, &base_probl, &savedl12, &savedl13, &savedl22, &savedl23, &dxx, 0)) {
 	break;
       }
       tprob += dxx;
       if (dir) {
-	if (savedr23 > savedr22) {
-	  savedr13 += 1;
-	  base_probr *= ((cur11 + 1) * savedr23) / (cur21 * savedr13);
-	  savedr23 -= 1;
-	} else {
+	if (savedr22) {
 	  savedr12 += 1;
 	  base_probr *= ((cur11 + 1) * savedr22) / (cur21 * savedr12);
 	  savedr22 -= 1;
+	} else {
+	  savedr13 += 1;
+	  base_probr *= ((cur11 + 1) * savedr23) / (cur21 * savedr13);
+	  savedr23 -= 1;
 	}
       } else {
-	if (savedr13 > savedr12) {
+	if (savedr13) {
 	  savedr23 += 1;
 	  base_probr *= ((cur21 + 1) * savedr13) / (cur11 * savedr23);
 	  savedr13 -= 1;
@@ -533,18 +544,17 @@ double fisher23(uint32_t m11, uint32_t m12, uint32_t m13, uint32_t m21, uint32_t
 	  savedr12 -= 1;
 	}
       }
-      tail_stop = tprob * DOUBLE_PREC_LIMIT;
       fisher23_tailsum(tail_stop, &base_probr, &savedr12, &savedr13, &savedr22, &savedr23, &dyy, 1);
       tprob += dyy;
       cprob += row_prob - dxx - dyy;
       if (cprob == INFINITY) {
 	return 0;
       }
+      tail_stop = tprob * DOUBLE_PREC_LIMIT;
     }
-    if (ukk) {
+    if (!ukk) {
       continue;
     }
-    tail_stop = tprob * DOUBLE_PREC_LIMIT;
     savedl12 += savedl13;
     savedl22 += savedl23;
     if (dir) {
