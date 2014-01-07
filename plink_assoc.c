484eeb7116890b69e8f02446ca363c769d3e30ca
Christopher Chang <chrchang523@gmail.com>
2014-01-07 18:02:55 +0800

    unused variable

--- plink_assoc.c
+++ plink_assoc.c
@@ -9015,7 +9015,6 @@ void calc_git_missing(uint32_t pheno_nm_ct, uint32_t perm_vec_ct, uintptr_t* __r
   uint32_t perm_ct32x4 = perm_ct32 * 4;
   uint32_t perm_ct8 = (perm_vec_ct + 7) / 8;
   uint32_t perm_ct4 = (perm_vec_ct + 3) / 4;
-  uint32_t perm_ct16x16 = 16 * perm_ct16;
   uint32_t* permsv = perm_vecst;
   uint32_t* gitv[3];
   uint32_t* git_merge4;
