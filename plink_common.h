dec4f1f70c63c9a40d713f992990235b39133f27
Christopher Chang <chrchang523@gmail.com>
2014-02-08 14:37:24 +0800

    temporarily block counterproductively high thread count while we improve multithread efficiency

--- plink_common.h
+++ plink_common.h
@@ -495,8 +495,8 @@
   #define MAX_THREADS_P1 64
 #else
 // shouldn't be larger than MODEL_BLOCKSIZE for now
-  #define MAX_THREADS 1023
-  #define MAX_THREADS_P1 1024
+  #define MAX_THREADS 63
+  #define MAX_THREADS_P1 64
 #endif
 
 #ifdef __LP64__
