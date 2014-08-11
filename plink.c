3b6d78b12e335ca476e4c2c5367b1401879d35ad
Christopher Chang <chrchang523@gmail.com>
2014-08-11 08:08:11 -0700

    align date

--- plink.c
+++ plink.c
@@ -98,7 +98,7 @@ const char ver_str[] =
   " 32-bit"
 #endif
   // include trailing space if day < 10, so character length stays the same
-  " (11 Aug 2014) ";
+  " (11 Aug 2014)";
 const char ver_str2[] =
 #ifdef STABLE_BUILD
   // " " // (don't want this when version number has a trailing letter)
