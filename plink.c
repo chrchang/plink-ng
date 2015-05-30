6387ea03ca0510b46715d0591160b2f36315fe92
Christopher Chang <chrchang523@gmail.com>
2015-05-29 22:26:33 -0700

    oops, today is the 29th

--- plink.c
+++ plink.c
@@ -103,7 +103,7 @@ const char ver_str[] =
 #else
   " 32-bit"
 #endif
-  " (28 May 2015)";
+  " (29 May 2015)";
 const char ver_str2[] =
   // include leading space if day < 10, so character length stays the same
   ""
