ab7a2357e665fe07756c4d76183c3b8e59cb7903
Christopher Chang <chrchang523@gmail.com>
2015-06-13 00:02:07 -0700

    fix declaration

--- plink_matrix.h
+++ plink_matrix.h
@@ -94,8 +94,8 @@ extern "C" {
               __CLPK_integer* liwork, __CLPK_integer* info);
 
   void dgesdd_(char* jobs, __CLPK_integer* m, __CLPK_integer* n,
-               __CLPK_doublereal* a, __CLPK_integer* a, __CLPK_integer* lda,
-               __CLPK_doublereal* s, __CLPK_doublereal* u, __CLPK_integer* ldu,
+               __CLPK_doublereal* a, __CLPK_integer* lda, __CLPK_doublereal* s,
+               __CLPK_doublereal* u, __CLPK_integer* ldu,
                __CLPK_doublereal* vt, __CLPK_integer* ldvt,
                __CLPK_doublereal* work, __CLPK_integer* lwork,
                __CLPK_integer* iwork, __CLPK_integer* info);
