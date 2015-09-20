b56742c68c5c016362b3b94280a739d53076ddb9
Christopher Chang <chrchang523@gmail.com>
2015-09-20 00:37:50 -0700

    set-but-not-used

--- plink_family.c
+++ plink_family.c
@@ -3116,7 +3116,7 @@ int32_t dfam(pthread_t* threads, FILE* bedfile, uintptr_t bed_offset, char* outn
   uintptr_t* size_one_sibships;
   double* maxt_extreme_stat = NULL;
   uint32_t mu_table[MODEL_BLOCKSIZE];
-  char* outname_end2;
+  // char* outname_end2;
   char* wptr;
   uint64_t* family_list;
   uint64_t* trio_list;
@@ -3531,7 +3531,7 @@ int32_t dfam(pthread_t* threads, FILE* bedfile, uintptr_t bed_offset, char* outn
     }
   }
 
-  outname_end2 = memcpyb(outname_end, ".dfam", 6);
+  // outname_end2 = memcpyb(outname_end, ".dfam", 6);
   if (fopen_checked(&outfile, outname, "w")) {
     goto dfam_ret_OPEN_FAIL;
   }
