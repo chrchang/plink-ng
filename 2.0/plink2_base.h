936ad456f788387e15ba16138453626f56a7751e
Christopher Chang <chrchang523@gmail.com>
2018-05-20 09:11:52 -0700

    freeBSD build patch

--- 2.0/plink2_base.h
+++ 2.0/plink2_base.h
@@ -448,7 +448,7 @@ typedef uint32_t BoolErr;
 #  define FOPEN_RB "r"
 #  define FOPEN_WB "w"
 #  define FOPEN_AB "a"
-#  ifdef __APPLE__
+#  if defined(__APPLE__) || defined(__FreeBSD__)
 #    define fread_unlocked fread
 #    define fwrite_unlocked fwrite
 #  endif
