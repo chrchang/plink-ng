09e9ac809329b1adb27cbb1922afdc92de9a55a7
Christopher Chang <chrchang523@gmail.com>
2020-01-03 15:46:37 -0800

    update year

--- 1.9/plink_filter.c
+++ 1.9/plink_filter.c
@@ -1,4 +1,4 @@
-// This file is part of PLINK 1.90, copyright (C) 2005-2019 Shaun Purcell,
+// This file is part of PLINK 1.90, copyright (C) 2005-2020 Shaun Purcell,
 // Christopher Chang.
 //
 // This program is free software: you can redistribute it and/or modify
--- 1.9/plink_set.h
+++ 1.9/plink_set.h
@@ -1,7 +1,7 @@
 #ifndef __PLINK_SET_H__
 #define __PLINK_SET_H__
 
-// This file is part of PLINK 1.90, copyright (C) 2005-2019 Shaun Purcell,
+// This file is part of PLINK 1.90, copyright (C) 2005-2020 Shaun Purcell,
 // Christopher Chang.
 //
 // This program is free software: you can redistribute it and/or modify
