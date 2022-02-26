7d497e2ae38452e5e571bb796ec519fe2828b9a3
Christopher Chang <chrchang523@gmail.com>
2022-02-26 14:16:58 -0800

    date

--- 1.9/plink.c
+++ 1.9/plink.c
@@ -105,7 +105,7 @@ static const char ver_str[] =
 #else
   " 32-bit"
 #endif
-  " (24 Feb 2022)";
+  " (26 Feb 2022)";
 static const char ver_str2[] =
   // include leading space if day < 10, so character length stays the same
   ""
