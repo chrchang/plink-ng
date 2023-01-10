5554933f6934084d3dfa1f7a580f75ff3adb4f41
Christopher Chang <chrchang523@gmail.com>
2023-01-10 12:31:47 -0500

    plink 1.9 2023 update

--- 1.9/plink_cluster.h
+++ 1.9/plink_cluster.h
@@ -1,7 +1,7 @@
 #ifndef __PLINK_CLUSTER_H__
 #define __PLINK_CLUSTER_H__
 
-// This file is part of PLINK 1.90, copyright (C) 2005-2022 Shaun Purcell,
+// This file is part of PLINK 1.90, copyright (C) 2005-2023 Shaun Purcell,
 // Christopher Chang.
 //
 // This program is free software: you can redistribute it and/or modify
--- 1.9/plink_ld.c
+++ 1.9/plink_ld.c
@@ -1,4 +1,4 @@
-// This file is part of PLINK 1.90, copyright (C) 2005-2022 Shaun Purcell,
+// This file is part of PLINK 1.90, copyright (C) 2005-2023 Shaun Purcell,
 // Christopher Chang.
 //
 // This program is free software: you can redistribute it and/or modify
