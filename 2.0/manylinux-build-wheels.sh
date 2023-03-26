06ed39ed1e69f64e25f5755805f82facd3abab48
Christopher Chang <chrchang523@gmail.com>
2023-03-25 17:06:19 -0700

    include upload command

--- 2.0/manylinux-build-wheels.sh
+++ 2.0/manylinux-build-wheels.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 set -exuo pipefail
+
+function repair_wheel {
+    wheel="$1"
+    if ! auditwheel show "$wheel"; then
+        echo "Skipping non-platform wheel $wheel"
+    else
+        auditwheel repair "$wheel" --plat manylinux2014_x86_64 -w wheelhouse/
+    fi
+}
+
 for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install -r Python/dev-requirements.txt
     "${PYBIN}/pip" wheel Python/ --no-deps -w wheelhouse/
 done
+
+for whl in wheelhouse/*.whl; do
+    repair_wheel "$whl"
+done
+
+/opt/python/cp311-cp311/bin/pip install twine
+/opt/python/cp311-cp311/bin/twine upload wheelhouse/*manylinux*
