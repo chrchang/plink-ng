6841808af9b31c7407432d362c84fd41648aee80
Christopher Chang <chrchang523@gmail.com>
2023-05-31 20:09:53 -0400

    a4.2: fix --read-freq behavior on allele mismatch

--- 2.0/plink2.cc
+++ 2.0/plink2.cc
@@ -44,7 +44,7 @@
 namespace plink2 {
 #endif
 
-static const char ver_str[] = "PLINK v2.00a4.1"
+static const char ver_str[] = "PLINK v2.00a4.2"
 #ifdef NOLAPACK
   "NL"
 #elif defined(LAPACK_ILP64)
@@ -72,7 +72,7 @@ static const char ver_str[] = "PLINK v2.00a4.1"
 #elif defined(USE_AOCL)
   " AMD"
 #endif
-  " (28 May 2023)";
+  " (31 May 2023)";
 static const char ver_str2[] =
   // include leading space if day < 10, so character length stays the same
   ""
