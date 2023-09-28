5478ad18a9b7716737622a43c48d14492531b1fe
Christopher Chang <chrchang523@gmail.com>
2023-09-28 13:53:39 -0700

    compress fixtures, add --export A

--- 2.0/Tests/TEST_ONE_WAY_EXPORT/run_tests.sh
+++ 2.0/Tests/TEST_ONE_WAY_EXPORT/run_tests.sh
@@ -6,6 +6,11 @@ set -exo pipefail
 # manually-sanity-checked ground truth files.
 
 $1/plink2 $2 $3 --pfile all_ploidy vzs --snps-only --export phylip --out plink2_test
+plink2 --zd plink2_test.phy.want.zst > plink2_test.phy.want
 diff -q plink2_test.phy plink2_test.phy.want
 $1/plink2 $2 $3 --pfile all_ploidy vzs --chr PAR1,X,PAR2 --keep-females --snps-only --export phylip-phased --out plink2_test.phased
+plink2 --zd plink2_test.phased.phy.want.zst > plink2_test.phased.phy.want
 diff -q plink2_test.phased.phy plink2_test.phased.phy.want
+$1/plink2 $2 $3 --pfile all_ploidy vzs --export A --out plink2_test
+plink2 --zd plink2_test.raw.want.zst > plink2_test.raw.want
+diff -q plink2_test.raw plink2_test.raw.want
