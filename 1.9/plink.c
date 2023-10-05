407a166663d3255b6c76269704b9eb7608189eb3
Christopher Chang <chrchang523@gmail.com>
2023-10-05 16:52:49 -0700

    help copyedit

--- 1.9/plink.c
+++ 1.9/plink.c
@@ -105,7 +105,7 @@ static const char ver_str[] =
 #else
   " 32-bit"
 #endif
-  " (6 Sep 2023)";
+  " (5 Oct 2023)";
 static const char ver_str2[] =
   // include leading space if day < 10, so character length stays the same
   " "
