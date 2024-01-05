a25590901e95d56a7e070e4fd07b1c92bb0ecb59
Christopher Chang <chrchang523@gmail.com>
2024-01-04 23:39:55 -0800

    2024

--- 2.0/include/plink2_bitmap.h
+++ 2.0/include/plink2_bitmap.h
@@ -1,7 +1,7 @@
 #ifndef __PLINK2_BITMAP_H__
 #define __PLINK2_BITMAP_H__
 
-// This library is part of PLINK 2.00, copyright (C) 2005-2023 Shaun Purcell,
+// This library is part of PLINK 2.00, copyright (C) 2005-2024 Shaun Purcell,
 // Christopher Chang.
 //
 // This library is free software: you can redistribute it and/or modify it
