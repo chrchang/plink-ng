953408c948d3cb02e4859bcc5215bf0de603deac
Gabriel Hoffman <gabriel.hoffman@mssm.edu>
2025-01-21 18:01:51 -0500

    add ffi and install.libs.R (#290)
    Co-authored-by: Gabriel Hoffman <gabriel.hoffman@gmail.com>

---
+++ 2.0/pgenlibr/src/install.libs.R
@@ -0,0 +1,5 @@
+files <- Sys.glob(paste0("*", SHLIB_EXT))
+files <- c(files, dir(pattern="a$"))
+dest <- file.path(R_PACKAGE_DIR, paste0('libs', R_ARCH))
+dir.create(dest, recursive = TRUE, showWarnings = FALSE)
+file.copy(files, dest, overwrite = TRUE)
