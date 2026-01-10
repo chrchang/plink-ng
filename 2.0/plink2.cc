0a841bf5da9d682ab32344981f8fa7af6e43661b
Christopher Chang <chrchang523@gmail.com>
2026-01-10 10:52:28 -0800

    mixed nonref_flags split/join was not walled off correctly

--- 2.0/plink2.cc
+++ 2.0/plink2.cc
@@ -44,7 +44,7 @@
 namespace plink2 {
 #endif
 
-static PREFER_CONSTEXPR char ver_str[] = "PLINK v2.0.0-a.5.38"
+static PREFER_CONSTEXPR char ver_str[] = "PLINK v2.0.0-a.5.38.a"
 #ifdef NOLAPACK
   "NL"
 #elif defined(LAPACK_ILP64)
@@ -72,10 +72,10 @@ static PREFER_CONSTEXPR char ver_str[] = "PLINK v2.0.0-a.5.38"
 #elif defined(USE_AOCL)
   " AMD"
 #endif
-  " (3 Jan 2026)";
+  " (10 Jan 2026)";
 static PREFER_CONSTEXPR char ver_str2[] =
   // include leading space if day < 10, so character length stays the same
-  " "
+  ""
 
 #ifdef NOLAPACK
 #elif defined(LAPACK_ILP64)
@@ -100,7 +100,7 @@ static PREFER_CONSTEXPR char ver_str2[] =
 #  endif
 #endif
 
-  "    cog-genomics.org/plink/2.0/\n"
+  "  cog-genomics.org/plink/2.0/\n"
   "(C) 2005-2026 Shaun Purcell, Christopher Chang    GNU General Public License v3\n";
 #ifdef HAS_CONSTEXPR
 static_assert(CompileTimeSlen(ver_str) + CompileTimeSlen(ver_str2) == 160, "ver_str/ver_str2 must be updated");
