ab0f62776c25940ab8547678187cdafc4560990c
Christopher Chang <chrchang523@gmail.com>
2026-03-30 17:13:59 -0700

    plink2 zlib 1.3.2

--- 2.0/plink2.cc
+++ 2.0/plink2.cc
@@ -90,7 +90,7 @@ static PREFER_CONSTEXPR char ver_str[] = "PLINK v2.0.0-a.7"
 #elif defined(USE_AOCL)
   " AMD"
 #endif
-  " (28 Mar 2026)";
+  " (30 Mar 2026)";
 static PREFER_CONSTEXPR char ver_str2[] =
   // include leading space if day < 10, so character length stays the same
   ""
