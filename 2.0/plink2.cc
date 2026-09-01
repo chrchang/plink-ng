1723be8d6ed5bc02eca5de4ca710eb6882a047e2
Christopher Chang <chrchang523@gmail.com>
2026-09-01 07:18:37 -0700

    a8

--- 2.0/plink2.cc
+++ 2.0/plink2.cc
@@ -62,7 +62,7 @@
 namespace plink2 {
 #endif
 
-static PREFER_CONSTEXPR char ver_str[] = "PLINK v2.0.0-a.7.4.a"
+static PREFER_CONSTEXPR char ver_str[] = "PLINK v2.0.0-a.8"
 #ifdef NOLAPACK
   "NL"
 #elif defined(LAPACK_ILP64)
@@ -90,10 +90,10 @@ static PREFER_CONSTEXPR char ver_str[] = "PLINK v2.0.0-a.7.4.a"
 #elif defined(USE_AOCL)
   " AMD"
 #endif
-  " (31 Aug 2026)";
+  " (1 Sep 2026)";
 static PREFER_CONSTEXPR char ver_str2[] =
   // include leading space if day < 10, so character length stays the same
-  ""
+  " "
 
 #ifdef NOLAPACK
 #elif defined(LAPACK_ILP64)
@@ -118,7 +118,7 @@ static PREFER_CONSTEXPR char ver_str2[] =
 #  endif
 #endif
 
-  "   cog-genomics.org/plink/2.0/\n"
+  "       cog-genomics.org/plink/2.0/\n"
   "(C) 2005-2026 Shaun Purcell, Christopher Chang    GNU General Public License v3\n";
 #ifdef HAS_CONSTEXPR
 static_assert(CompileTimeSlen(ver_str) + CompileTimeSlen(ver_str2) == 160, "ver_str/ver_str2 must be updated");
