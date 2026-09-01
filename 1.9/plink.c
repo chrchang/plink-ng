6589775f3b88f7287f5fc1b895faad93148ebe05
Christopher Chang <chrchang523@gmail.com>
2026-09-01 04:11:25 -0700

    oops, wrong version-number-based length adjustment

--- 1.9/plink.c
+++ 1.9/plink.c
@@ -114,14 +114,14 @@ static const char ver_str2[] =
   // include leading space if day < 10, so character length stays the same
   " "
 #ifdef STABLE_BUILD
-  " " // adjust based on length of version number
+  "   " // adjust based on length of version number
 #else
   "      "
 #endif
 #ifndef NOLAPACK
   "  "
 #endif
-  "        cog-genomics.org/plink/1.9/\n"
+  "      cog-genomics.org/plink/1.9/\n"
   "(C) 2005-2026 Shaun Purcell, Christopher Chang   GNU General Public License v3"
 #if SPECIES_DEFAULT > 0
   "\nRecompiled with default species = "
