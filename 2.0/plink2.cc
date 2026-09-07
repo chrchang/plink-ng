9eea2b1c8ed9e25a247e24d4af897d5ca12ac891
Christopher Chang <chrchang523@gmail.com>
2026-09-07 06:15:07 -0700

    update date

--- 2.0/plink2.cc
+++ 2.0/plink2.cc
@@ -90,7 +90,7 @@ static PREFER_CONSTEXPR char ver_str[] = "PLINK v2.0.0-b.1"
 #elif defined(USE_AOCL)
   " AMD"
 #endif
-  " (5 Sep 2026)";
+  " (7 Sep 2026)";
 static PREFER_CONSTEXPR char ver_str2[] =
   // include leading space if day < 10, so character length stays the same
   " "
